f5bc22bf06eaa91ff4902223730436cf33fcc2c9
ribeirokzf <nvmreflex@gmail.com>
2024-11-21 23:35:19 -0300

    terminando o section news

--- header.js
+++ header.js
@@ -25,3 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+/* botao back-to-top */
+    var btn =documentSelector("#back-to-top");
+
+    btn.addEventListener("click",function(){
+        window.scrollTo(0,0);
+    });
